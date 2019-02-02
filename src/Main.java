59268926643f570e07e8301817e4b658a178b6e1
Maczi01 <system11>
2019-02-02 16:25:07 +0100

    Add second method

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(NumberHelper.sumaCyfr(1234));
-
+        System.out.println(NumberHelper.sumaCyfr2(11234));
+        
     }
 }
--- src/NumberHelper.java
+++ src/NumberHelper.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class NumberHelper {
 
     static int sumaCyfr(int liczba) {
@@ -8,5 +10,34 @@ public class NumberHelper {
             suma = suma + Character.getNumericValue(name.charAt(i));
         }
         return suma;
+    }
+
+    static int sumaCyfr2(int liczba) {
+        int suma = 0;
+        counter(liczba);
+        int[] tab = new int[counter(liczba)];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = liczba % 10;
+            liczba = liczba / 10;
+        }
+        suma = sumFromArray(tab);
+        return suma;
+    }
+
+    static int counter(int liczba) {
+        int count = 0;
+        while (liczba > 0) {
+            liczba = liczba / 10;
+            count++;
+        }
+        return count;
+    }
+
+    static int sumFromArray(int[] tablica) {
+        int sum = 0;
+        for (int s : tablica){
+            sum = sum + s;
+        }
+        return sum;
     }
 }
